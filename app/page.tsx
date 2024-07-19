7c0b4be5fc81b948781b298be6a472c54ebf24fe
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2024-07-19 22:29:37 +0900

    fix: use dvh instead of vh

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const HomeContainer = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    height: 100vh;
+    height: 100dvh;
 `;
 
 const Title = styled.h1`
